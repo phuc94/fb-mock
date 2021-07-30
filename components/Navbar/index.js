a89b310a69a6836484043c64bfbe8dd36081b3f7
Nguyen Truong Trung Phuc <52532736+phuc94@users.noreply.github.com>
2021-07-30 13:11:49 +0700

    update

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -4,17 +4,17 @@ import Link from "next/link";
 function Nav (){
     return (
         <div className="flex justify-between px-6 h-10 bg-blue-500">
-            <div class="pt-2">
+            <div className="pt-2">
                 <Link href="/about">
                     <a title='About'>About</a>
                 </Link>
             </div>
-            <div class="pt-2">
+            <div className="pt-2">
                 <Link href="/about">
                     <a title='About'>About</a>
                 </Link>
             </div>
-            <div class="pt-2">
+            <div className="pt-2">
                 <Link href="/about">
                     <a title='About'>About</a>
                 </Link>
